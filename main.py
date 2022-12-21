624cdfc724914f527b138a4dbe3cc8aa9bba00f5
Thilan Maduranga <thilan@zeldivalanka.com>
2022-12-21 14:47:55 +0530

    rebuild connections

--- main.py
+++ main.py
@@ -2,6 +2,36 @@ import pyautogui as pa
 import time
 
 time.sleep(5)
-for i in range(1, 501):
-    pa.write(str(i))
+
+
+def pressing_tab_key():
+    pa.keyDown("tab")
+    pa.keyUp("tab")
+
+
+def selecting_san_kote():
+    pa.keyDown("shift")
+    pa.keyDown("2")
+    pa.keyUp("2")
+    pa.keyUp("shift")
+    pa.write("holmana")
+
+
+def lover_emoji():
+    pa.write(":lover")
+    pressing_tab_key()
+
+
+def red_heart_emoji():
+    pa.write(":red heart")
+    pressing_tab_key()
+
+
+for i in range(1, 101):
+    selecting_san_kote()
+    pressing_tab_key()
+    pa.write("lv u sudu menika ")
+    lover_emoji()
+    lover_emoji()
+    red_heart_emoji()
     pa.press("enter")
